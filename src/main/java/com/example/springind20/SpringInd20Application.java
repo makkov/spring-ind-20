eee89970f56b29b2d128cabd789fdd1bd0849f9e
makkov <maxim_86nva@mail.ru>
2024-03-09 09:56:56 +0300

    init project

---
+++ src/main/java/com/example/springind20/SpringInd20Application.java
@@ -0,0 +1,13 @@
+package com.example.springind20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInd20Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInd20Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springind20/SpringInd20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springind20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInd20ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
